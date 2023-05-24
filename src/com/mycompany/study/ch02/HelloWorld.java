12613e246994111b6cdeaab4d6f9fb54bb000f88
ArtVandelay500 <ArtVandelay500@users.noreply.github.com>
2023-05-24 15:37:18 +0900

    dfdf

--- src/com/mycompany/study/ch02/HelloWorld.java
+++ src/com/mycompany/study/ch02/HelloWorld.java
@@ -1,9 +1,14 @@
 package com.mycompany.study.ch02;
 
 public class HelloWorld {
-	
-	public static void main(String[] args) {
-		System.out.println("HelloWorld");
-	}
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+		System.out.println("My Name is Hyun");
+		System.out.println("AGE : 28");
+		System.out.println("Nationality : Korea");
+		
+	}
+  
 }
